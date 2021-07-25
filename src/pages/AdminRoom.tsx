710ab7c4451e25dca6b3ae1d1f20d4962b0f9eb5
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-25 19:16:22 -0300

    linhas não usadas removidas

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,12 +1,9 @@
-import { FormEvent, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
-import emptyQuestions from '../assets/images/empty-questions.svg'
 import { Button } from '../components/Button/Button'
 import { RoomCode } from '../components/RoomCode/RoomCode'
-import { useAuth } from '../hooks/useAuth'
 import { database } from '../services/firebase'
 import '../styles/room.scss'
 import { Question } from '../components/Question/Question'
@@ -17,36 +14,10 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-	const { user } = useAuth()
 	const params = useParams<RoomParams>()
 	const roomId = params.id
-	const [newQuestion, setNewQuestion] = useState('')
 	const { questions, title } = useRoom(roomId)
 	const history = useHistory()
-
-	async function handleSendQuestion(event: FormEvent) {
-		event.preventDefault()
-
-		if (newQuestion.trim() === '')
-			return
-
-		if (!user)
-			throw new Error('Não há um usuário')
-
-		const question = {
-			content: newQuestion,
-			author: {
-				name: user.name,
-				avatar: user.avatar
-			},
-			isHighlighted: false,
-			isAnswered: false
-		}
-
-		await database.ref(`rooms/${roomId}/questions`).push(question)
-
-		setNewQuestion('')
-	}
 
 	async function handleDeleteQuestion(questionId: string){
 		if (window.confirm('Tem certeza que deseja excluir essa pergunta?')){
@@ -80,29 +51,6 @@ export function AdminRoom() {
 					{ questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
 				</div>
 
-				<form onSubmit={handleSendQuestion}>
-					<textarea
-						placeholder="O que você deseja perguntar?"
-						onChange={event => setNewQuestion(event.target.value)}
-						value={newQuestion}
-					/>
-
-					<div className="form-footer">
-						{
-							user ? (
-								<div className="user-info">
-									<img src={user.avatar} alt={user.name} />
-									<span>{user.name}</span>
-								</div>
-							) : (
-								<span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-							)
-						}
-						
-						<Button type="submit" disabled={!user}>Enviar pergunta</Button>
-					</div>
-				</form>
-
 				<div className="question-list">
 					{
 						questions.map(question => {
@@ -123,16 +71,6 @@ export function AdminRoom() {
 						})
 					}
 				</div>
-
-				{
-					questions.length === 0 ? (
-						<div className="no-questions">
-							<img src={emptyQuestions} alt="Não há perguntas" />
-							<h2 className="title">Nenhuma pergunta por aqui...</h2>
-							<p className="subtitle">Seja a primeira pessoa a fazer uma pergunta!</p>
-						</div>
-					) : null
-				}
 			</main>
 		</div>
 	)
