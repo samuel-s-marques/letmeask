45f8dd49ab8f4c73ee890819e6e8c524c923feff
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-26 16:27:24 -0300

    implementado modal de encerrar sala

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,6 @@
 import { useHistory, useParams } from 'react-router-dom'
+import Modal from 'react-modal'
+import { useState } from 'react'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
@@ -10,6 +12,7 @@ import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
 import { database } from '../services/firebase'
 import '../styles/room.scss'
+import '../styles/modal.scss'
 import { Question } from '../components/Question'
 import { useRoom } from '../hooks/useRoom'
 
@@ -22,6 +25,7 @@ export function AdminRoom() {
 	const roomId = params.id
 	const { questions, title } = useRoom(roomId)
 	const history = useHistory()
+	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	async function handleDeleteQuestion(questionId: string){
 		if (window.confirm('Tem certeza que deseja excluir essa pergunta?')){
@@ -56,7 +60,7 @@ export function AdminRoom() {
 					<img src={logoImg} alt="Logo letmeask" />
 					<div>
 						<RoomCode code={roomId} />
-						<Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+						<Button isOutlined onClick={() => setIsModalOpen(true)}>Encerrar sala</Button>
 					</div>
 				</div>
 			</header>
@@ -121,6 +125,74 @@ export function AdminRoom() {
 					) : null
 				}
 			</main>
+
+			<Modal
+				isOpen={isModalOpen}
+				ariaHideApp={false}
+				onRequestClose={() => setIsModalOpen(false)}
+				contentLabel="Deseja mesmo encerrar a sala?"
+				style={{
+					overlay: {
+						position: 'fixed',
+						inset: 0,
+						backgroundColor: 'rgba(5, 2, 6, 0.8)',
+					},
+					content: {
+						height: '362px',
+						width: '590px',
+						borderRadius: '8px',
+						margin: 'auto',
+						backgroundColor: '#F8F8F8'
+					}
+				}}
+			>
+				<div className="excludeModal">
+					<svg
+						width="48"
+						height="48"
+						viewBox="0 0 48 48"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M29.66 18.3398L18.34 29.6598"
+							stroke="#E73F5D"
+							strokeWidth="4"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						/>
+						<path
+							d="M29.66 29.6598L18.34 18.3398"
+							stroke="#E73F5D"
+							strokeWidth="4"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						/>
+						<path
+							fillRule="evenodd"
+							clipRule="evenodd"
+							d="M24 42V42C14.058 42 6 33.942 6 24V24C6 14.058 14.058 6 24 6V6C33.942 6 42 14.058 42 24V24C42 33.942 33.942 42 24 42Z"
+							stroke="#E73F5D"
+							strokeWidth="4"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						/>
+					</svg>
+
+					<h2>Encerrar sala</h2>
+					<p>Tem certeza que você deseja encerrar esta sala?</p>
+
+					<div>
+						<button type="button" id="cancelar" onClick={() => setIsModalOpen(false)}>
+							Cancelar
+						</button>
+
+						<button type="button" id="encerrar" onClick={handleEndRoom}>
+							Sim, encerrar
+						</button>
+					</div>
+				</div>
+			</Modal>
 		</div>
 	)
 }
