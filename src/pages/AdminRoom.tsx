69375bb17b07507ac58fa1d9ef31defe6d6ee626
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-26 13:20:36 -0300

    adicionadas funções de resposta e marcação

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,9 @@ import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
+
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
 import { database } from '../services/firebase'
@@ -33,6 +36,18 @@ export function AdminRoom() {
 		history.push('/')
 	}
 
+	async function handleCheckQuestion(questionId: string) {
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+			isAnswered: true
+		})
+	}
+
+	async function handleAnswerQuestion(questionId: string) {
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+			isHighlighted: true
+		})
+	}
+
 	return (
 		<div id="page-room">
 			<header>
@@ -59,7 +74,30 @@ export function AdminRoom() {
 									key={question.id}
 									content={question.content}
 									author={question.author}
+									isAnswered={question.isAnswered}
+									isHighlighted={question.isHighlighted}
 								>
+									{
+										!question.isAnswered && (
+											<>
+												<button
+													type="button"
+													onClick={() => handleCheckQuestion(question.id)}
+												>
+													<img src={checkImg} alt="Marcar pergunta como respondida" />
+												</button>
+
+												<button
+													type="button"
+													onClick={() => handleAnswerQuestion(question.id)}
+												>
+													<img src={answerImg} alt="Responder pergunta" />
+												</button>
+											</>
+										)
+									}
+
+
 									<button
 										type="button"
 										onClick={() => handleDeleteQuestion(question.id)}
