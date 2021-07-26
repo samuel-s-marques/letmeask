6a523cd40879c3c2745465b1798042dc31f29528
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-26 14:21:07 -0300

    adicionado toast

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,5 +1,6 @@
 import copyImg from '../../assets/images/copy.svg'
 import './styles.scss'
+import toast, { Toaster } from 'react-hot-toast'
 
 type RoomCodeProps = {
 	code: string;
@@ -8,14 +9,27 @@ type RoomCodeProps = {
 export function RoomCode(props: RoomCodeProps) {
 	function copyRoomCodeToClipboard() {
 		navigator.clipboard.writeText(props.code)
+		.then(() => {
+			toast.success('Código copiado!')
+		})
+		.catch(() => {
+			toast.error('Não foi possível copiar o código!')
+		})
 	}
 
 	return (
-		<button className="room-code" onClick={copyRoomCodeToClipboard}>
-			<div>
-				<img src={copyImg} alt="Copy room code" />
-			</div>
-			<span>Sala #{props.code}</span>
-		</button>
+		<>
+			<Toaster 
+				position="top-right"
+				reverseOrder={false}
+			/>
+
+			<button className="room-code" onClick={copyRoomCodeToClipboard}>
+				<div>
+					<img src={copyImg} alt="Copy room code" />
+				</div>
+				<span>Sala #{props.code}</span>
+			</button>
+		</>
 	)
 }
