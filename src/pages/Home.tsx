88631aeff845b9298e718e3456998c7e6e6b344f
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-26 10:34:08 -0300

    adicionado toast

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -8,10 +9,13 @@ import loginImg from '../assets/images/log-in.svg'
 import '../styles/auth.scss'
 import { Button } from '../components/Button/Button'
 import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+import toast, { Toaster } from 'react-hot-toast'
 
 export function Home() {
 	const history = useHistory()
 	const { user, signInWithGoogle } = useAuth()
+	const [roomCode, setRoomCode] = useState('')
 
 	async function handleCreateRoom() {
 		if (!user)
@@ -20,8 +24,35 @@ export function Home() {
 		history.push('/rooms/new')
 	}
 
+	async function handleJoinRoom(event: FormEvent) {
+		event.preventDefault()
+
+		if (roomCode.trim() === '')
+			return
+		
+		const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+		if (!roomRef.exists()) {
+			toast.error('Esta sala não existe.')
+			return
+		}
+
+		if (roomRef.val().endedAt) {
+			toast.error('Esta sala já foi fechada.')
+			return
+		}
+		
+		history.push(`/rooms/${roomCode}`)
+	}
+
 	return (
 		<div id="page-auth">
+			<div>
+				<Toaster 
+					position="top-right"
+					reverseOrder={false}
+				/>
+			</div>
 			<aside>
 				<img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
 				<strong>Crie salas de Q&amp;A ao vivo</strong>
@@ -36,10 +67,12 @@ export function Home() {
 						Crie sua sala com o Google
 					</button>
 					<div className="separator">ou entre em uma sala</div>
-					<form>
+					<form onSubmit={handleJoinRoom}>
 						<input
 							type="text"
 							placeholder="Digite o código da sala"
+							onChange={event => setRoomCode(event.target.value)}
+							value={roomCode}
 						/>
 						<Button type="submit">
 							<img src={loginImg} alt="Ícone de log-in" />
