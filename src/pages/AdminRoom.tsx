cea970aaf7f3d94c225a40ea02915f69788cb7e1
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-08-01 17:15:07 -0300

    adicionado modal de deletar pergunta

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -3,9 +3,6 @@ import Modal from 'react-modal'
 import { useState } from 'react'
 
 import logoImg from '../assets/images/logo.svg'
-import deleteImg from '../assets/images/delete.svg'
-import checkImg from '../assets/images/check.svg'
-import answerImg from '../assets/images/answer.svg'
 import emptyQuestions from '../assets/images/empty-questions.svg'
 
 import { Button } from '../components/Button'
@@ -26,11 +23,12 @@ export function AdminRoom() {
 	const { questions, title } = useRoom(roomId)
 	const history = useHistory()
 	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [isDeleteQuestionModalOpen, setIsDeleteQuestionModalOpen] = useState(false)
+	const [questionId, setQuestionId ] = useState('')
 
 	async function handleDeleteQuestion(questionId: string){
-		if (window.confirm('Tem certeza que deseja excluir essa pergunta?')){
-			await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
-		}
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+		setIsDeleteQuestionModalOpen(false)
 	}
 
 	async function handleEndRoom() {
@@ -88,15 +86,22 @@ export function AdminRoom() {
 												<button
 													type="button"
 													onClick={() => handleCheckQuestion(question.id)}
+													className="check-button"
 												>
-													<img src={checkImg} alt="Marcar pergunta como respondida" />
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<circle cx="12.0003" cy="11.9998" r="9.00375" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+														<path d="M8.44287 12.3391L10.6108 14.507L10.5968 14.493L15.4878 9.60193" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+													</svg>
 												</button>
 
 												<button
 													type="button"
 													onClick={() => handleAnswerQuestion(question.id)}
+													className="answer-button"
 												>
-													<img src={answerImg} alt="Responder pergunta" />
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<path fillRule="evenodd" clipRule="evenodd" d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+													</svg>
 												</button>
 											</>
 										)
@@ -105,9 +110,13 @@ export function AdminRoom() {
 
 									<button
 										type="button"
-										onClick={() => handleDeleteQuestion(question.id)}
+										onClick={() => { setIsDeleteQuestionModalOpen(true); setQuestionId(question.id) }}
+										className="delete-button"
 									>
-										<img src={deleteImg} alt="Remover pergunta" />
+										<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M3 5.99988H5H21" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+											<path d="M8 5.99988V3.99988C8 3.46944 8.21071 2.96074 8.58579 2.58566C8.96086 2.21059 9.46957 1.99988 10 1.99988H14C14.5304 1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 2.96074 16 3.46944 16 3.99988V5.99988M19 5.99988V19.9999C19 20.5303 18.7893 21.039 18.4142 21.4141C18.0391 21.7892 17.5304 21.9999 17 21.9999H7C6.46957 21.9999 5.96086 21.7892 5.58579 21.4141C5.21071 21.039 5 20.5303 5 19.9999V5.99988H19Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+										</svg>
 									</button>
 								</Question>
 							)
@@ -183,6 +192,59 @@ export function AdminRoom() {
 
 						<button type="button" id="encerrar" onClick={handleEndRoom}>
 							Sim, encerrar
+						</button>
+					</div>
+				</div>
+			</Modal>
+
+			<Modal
+				isOpen={isDeleteQuestionModalOpen}
+				ariaHideApp={false}
+				onRequestClose={() => setIsDeleteQuestionModalOpen(false)}
+				contentLabel="Deseja mesmo excluir a pergunta?"
+				style={{
+					overlay: {
+						position: 'fixed',
+						inset: 0,
+						backgroundColor: 'rgba(5, 2, 6, 0.8)',
+					}
+				}}
+				className="Modal"
+			>
+				<div className="excludeModal">
+					<svg
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M3 5.99988H5H21"
+							stroke="#E73F5D"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						/>
+						<path
+							d="M8 5.99988V3.99988C8 3.46944 8.21071 2.96074 8.58579 2.58566C8.96086 2.21059 9.46957 1.99988 10 1.99988H14C14.5304 1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 2.96074 16 3.46944 16 3.99988V5.99988M19 5.99988V19.9999C19 20.5303 18.7893 21.039 18.4142 21.4141C18.0391 21.7892 17.5304 21.9999 17 21.9999H7C6.46957 21.9999 5.96086 21.7892 5.58579 21.4141C5.21071 21.039 5 20.5303 5 19.9999V5.99988H19Z"
+							stroke="#E73F5D"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						/>
+					</svg>
+
+					<h2>Excluir pergunta</h2>
+					<p>Tem certeza que você deseja excluir esta pergunta?</p>
+
+					<div>
+						<button type="button" id="cancelar" onClick={() => setIsDeleteQuestionModalOpen(false)}>
+							Cancelar
+						</button>
+
+						<button type="button" id="encerrar" onClick={() => handleDeleteQuestion(questionId)}>
+							Sim, excluir
 						</button>
 					</div>
 				</div>
